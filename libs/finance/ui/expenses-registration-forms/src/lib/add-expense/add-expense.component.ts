7911d01379e2a14b044727573cf75b169be0a3a5
Jigme Thukten <jme@PC-M.local>
2024-10-29 21:23:57 -0400

    feat(): template driven form complete

--- libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.component.ts
+++ libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewCh
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AddExpense } from './add-expense.interface';
+import { MaxWordCountDirective } from '@bt-libs/shared/util/form-validators';
 
 @Component({
   selector: 'bt-libs-ui-add-expense-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MaxWordCountDirective],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
