62baa652dcc9ad5968631ed3b39d53caea7bd666
Jigme Thukten <jme@PC-M.local>
2024-10-29 23:15:20 -0400

    feat(): reactive form

--- libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.component.ts
+++ libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.component.ts
@@ -1,32 +1,52 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
-import { AddExpense } from './add-expense.interface';
-import { MaxWordCountDirective } from '@bt-libs/shared/util/form-validators';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddExpenseReactive } from './add-expense.interface';
+import { MaxWordCountDirective, maxWordCountValidator } from '@bt-libs/shared/util/form-validators'
 
 @Component({
   selector: 'bt-libs-ui-add-expense-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, MaxWordCountDirective],
+  imports: [CommonModule, ReactiveFormsModule, MaxWordCountDirective],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddExpenseComponent {
 
-  @Input() expenseToAdd: AddExpense = {
-    description: '',
-    amountExclVat: null,
-    vatPercentage: null,
-    date: null,
+  @Input() public set expenseToAdd(value: AddExpenseReactive) {
+    this.addExpenseForm.patchValue(value);
+    this.addExpenseForm.controls.tags.clear();
+    value?.tags?.forEach(tag => {
+      this.addExpenseForm.controls.tags.push(new FormControl(tag));
+    })
   }
 
-  @ViewChild('addExpenseForm') form!: NgForm;
-  @Output() addExpense = new EventEmitter<AddExpense>();
+  @Output() addExpense = new EventEmitter<AddExpenseReactive>();
 
-  onSubmit(): void {
-    this.addExpense.emit(structuredClone(this.expenseToAdd));
-    this.form.reset();
+
+  addExpenseForm = new FormGroup({
+    description: new FormControl('', [Validators.required, maxWordCountValidator(3)]),
+    amount: new FormGroup({
+      amountExclVat: new FormControl<number | null>(null),
+      vatPercentage: new FormControl<number | null>(null),
+    }),
+    date: new FormControl(['']),
+    tags: new FormArray([ new FormControl('') ])
+  });
+
+  addTag():void {
+    this.addExpenseForm.controls.tags.insert(0, new FormControl(''));
+  }
+
+  removeTag(index: number) {
+    this.addExpenseForm.controls.tags.removeAt(index)
+  }
+
+  onSubmit() {
+    console.log(this.addExpenseForm.value)
+    this.addExpense.emit(structuredClone(this.addExpenseForm.value as AddExpenseReactive));
+    this.addExpenseForm.reset();
   }
 
   
--- libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.interface.ts
+++ libs/finance/ui/expenses-registration-forms/src/lib/add-expense/add-expense.interface.ts
@@ -4,3 +4,13 @@ export interface AddExpense {
     vatPercentage: number | null;
     date: Date | null;
 }
+
+export interface AddExpenseReactive {
+    description?: string;
+    amount?: {
+        amountExclVat?: number | null;
+        vatPercentage?: number | null;
+    };
+    date?: string[];
+    tags: string[];
+}
